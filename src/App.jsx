440a1efffb5f78351c5903e1982e8cd2d86789d2
Alexander Spatar <younax@yandex.ru>
2022-10-19 14:14:30 +0300

    BP-1: Initialize project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './styles/index.scss';
+// import { Routes, Route } from 'react-router-dom';
+
+export default function App() {
+  return (
+    <>
+      <h1>6 Pet Projects</h1>
+      <div className="buttons">
+        <Routes>
+
+        </Routes>
+      </div>
+    </>
+  );
+}
