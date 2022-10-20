3da212e69390d3e45148c0435d0feac6e35ab457
Alexander Spatar <younax@yandex.ru>
2022-10-20 13:55:38 +0300

    BP-6: UserList prepared

--- src/pages/user-list-page.jsx
+++ src/pages/user-list-page.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+
+
 
 export default function UserList() {
-  return (
-    <div>UserList</div>
-  )
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://reqres.in/api/users')
+      .then((res) => res.json())
+      .then((json) => setUsers(json.data))
+      .catch(err => {
+        console.warn(err);
+        alert('Data is not received')
+      })
+  }, []);
+
+  return <div>UserList</div>;
 }
